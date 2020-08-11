a933aa1dfb6e7056445680902231ea1100471e34
jinnniu <65542045+jinnniu@users.noreply.github.com>
2020-08-11 20:59:04 +0900

    day1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Button, Progress} from 'semantic-ui-react'
 
 function App() {
   return (
+    <div>
     <p>Yuu Jin, Jeong</p>
+    <Button Primary>Hi</Button>
+    <Progress percent = {33} indicating>
+    <div>
   );
 }
 
